441eec055a51cebe156548100f571ea91d48edc6
gkiar <gkiar07@gmail.com>
2016-05-24 12:49:04 -0400

    updated naming

---
+++ Python/fibers/3d_fibers.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# 3d_fibers.py
+# Created by Greg Kiar on 2016-05-24.
+# Email: gkiar@jhu.edu
+
+from argparse import ArgumentParser
+from dipy.viz import window, actor
+import numpy as np
+
+def visualize(fibers, outf=None):
+    """
+    Takes fiber streamlines and visualizes them using DiPy
+
+    Required Arguments:
+        - fibers:
+            fiber streamlines in a list as returned by DiPy
+    Optional Arguments:
+        - save:
+            flag indicating whether or not you want the image saved
+            to disk after being displayed
+    """
+    # Initialize renderer
+    renderer = window.Renderer()
+
+    # Add streamlines as a DiPy viz object
+    stream_actor = actor.line(fibers)
+
+    # Set camera orientation properties
+    # TODO: allow this as an argument
+    renderer.set_camera()
+                        # position=(-176.42, 118.52, 128.20),
+                        # focal_point=(113.30, 128.31, 76.56),
+                        # view_up=(0.18, 0.00, 0.98))
+
+    # Add streamlines to viz session
+    renderer.add(stream_actor)
+
+    # Display fibers
+    # TODO: allow size of window as an argument
+    window.show(renderer, size=(600, 600), reset_camera=False)
+
+    # Saves file, if you're into that sort of thing...
+    if outf is not None:
+        window.record(renderer, out_path=outf, size=(600, 600))
+
+def main():
+    parser = ArgumentParser(description="Creates 3D vizualization of fibers")
+    parser.add_argument("fibers", action="store", help="npz format fibers")
+    parser.add_argument("--outf", "-o", action="store", default=None,
+                        help="file for image")
+    result = parser.parse_args()
+
+    fibs = np.load(result.fibers)
+    fibs = fibs[fibs.keys()[0]]
+
+    if result.outf is not None:
+        visualize(fibs, result.outf)
+    else:
+        visualize(fibs)
+
+if __name__ == "__main__":
+    main()
---
+++ Python/fibers/README.md
@@ -0,0 +1,43 @@
+## 3D Fiber Visualization
+
+This script provides a 3D interactive plot of fiber streamlines for manipulation
+and optionally saves the images to disk. It can either be called directly using
+a prompt from the commandline or imported into a python session and called inline
+in your Python script.
+
+
+#### Calling from Commandline
+
+The expected format of the fibers is a *numpy* `.npz` file which contains solely
+the streamlines as produced by DiPy (i.e. a list of lists of points along each
+fiber). Without the `-o` flag an interactive window will pop up and you can manipulate
+the fibers until you are happy and then close it. With the `-o` flag, when you close
+your window the image in its final state will be saved to the desired filename.
+
+Using the files in this directory, the code has been run like this:
+```
+python 3d_fibers.py fibers.npz -o fibers.png
+```
+
+#### Calling from within a script
+
+In your python script you can call it much like you would expect: you simply import
+the library and then call the desired function. An example of using this is
+shown here:
+
+```{python}
+from 3d_fibers import visualize
+
+fibers =   # your fibers here....
+outf =     # your outfile name here....
+
+if outf is not None:
+    visualize(fibers, outf)
+else:
+    visualize(fibers)
+
+```
+
+
+P.s. The fiber file attached is heavily downsampled in order to reduce the time
+of computation for the demo and the amount of data I needed in the repository.
