b75336f25523efb999d41fd93ea2a4218dc2c507
gkiar <gkiar07@gmail.com>
2017-03-21 10:42:27 -0400

    updated graph plotting

--- R/graphs/3dplot/graph3Dplot.R
+++ R/graphs/3dplot/graph3Dplot.R
@@ -1,5 +1,5 @@
 library(igraph)
-
+library(rgl)
 g <- read.graph('./demo_graph.graphml', format='graphml')
 
 centroids <- get.vertex.attribute(g, 'centroid')
@@ -12,9 +12,11 @@ for (i in 1:length(centroids)) {
 
 g$layout <- layouts
 
+open3d()
+bg3d('black')
 rglplot(g, layout = g$layout,
         vertex.label = NA, vertex.size = 12, vertex.color='tomato1',
         edge.width = log10(get.edge.attribute(g, 'weight')), edge.color='paleturquoise1')
 
 rgl.postscript('./demo_fig.pdf', fmt = 'pdf')
-
+rgl.snapshot('./demo_fig.png', fmt='png')
